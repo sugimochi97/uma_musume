9ce077c048b43b6fa997ba6a1adf695b9bd8641d
sugimochi97 <tashinoki.1019@gmail.com>
2021-05-18 17:35:06 +0900

    レース一覧をスクレイピングするソースコード、URLやクラスを非公開にする

---
+++ get_race.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+from scraping import *
+
+load_url = URL
+html = requests.get(load_url)
+soup = BeautifulSoup(html.content, 'html.parser')
+
+csv_file = open('./race.csv', 'w', newline='', encoding='shift-jis')
+csv_writer = csv.writer(csv_file)
+
+
+for element in soup.find_all(class_=RACE_CLASS):
+    lis = [i.text for i in element.find_all('td')]
+    lis = lis[0].split(' ') + lis[1].split(' / ')
+    csv_writer.writerow(lis)
+
+csv_file.close()
