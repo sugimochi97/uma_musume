fac5b9ae0442da90ffe94e42339548d7a9dbd6c6
sugimochi97 <tashinoki.1019@gmail.com>
2021-05-21 19:56:12 +0900

    綺麗にスクレイピングできるように処理を変更、使用ライブラリをcsvからpandasに変更

--- get_race.py
+++ get_race.py
@@ -1,19 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
+import pandas as pd
 from scraping import *
+
+RACE_COLUMNS = ['年', '月', 'レース名', 'クラス', '場所', '距離', '回り']
+OUTPUT_CSV = './race.csv'
 
 load_url = URL
 html = requests.get(load_url)
 soup = BeautifulSoup(html.content, 'html.parser')
 
-csv_file = open('./race.csv', 'w', newline='', encoding='utf-8')
-csv_writer = csv.writer(csv_file)
-
-
+load_url = URL
+html = requests.get(load_url)
+soup = BeautifulSoup(html.content, 'html.parser')
+lis_td = []
+lis_a = []
 for element in soup.find_all(class_=RACE_CLASS):
     lis = [i.text for i in element.find_all('td')]
-    lis = lis[0].split(' ') + lis[1].split(' / ')
-    csv_writer.writerow(lis)
-
-csv_file.close()
+    lis_td.append(lis[0].split(' ') + lis[1].split(' / '))
+    lis_a.append([i.text for i in element.find_all('a')])
+while [] in lis_a:              # 空リストを削除する
+    i = lis_a.index([])
+    lis_a.pop(i)
+    lis_td.pop(i)
+lis_a = [i[1] for i in lis_a]
+df = pd.DataFrame(lis_td, columns=RACE_COLUMNS)
+se = pd.Series(lis_a)
+df['レース名'] = se
+df.to_csv(OUTPUT_CSV)
