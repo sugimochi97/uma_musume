d658c586f0d19e5cff3eb1b8344fe31482108240
sugimochi97 <tashinoki.1019@gmail.com>
2021-05-21 11:24:39 +0900

    レース検索機能の追加

--- race_rote.py
+++ race_rote.py
@@ -1,20 +1,36 @@
+import csv
 import pandas as pd
 from tkinter import *
 
 PATH='race.csv'
+RACE_COLUMNS = ['年', '月', 'レース名', 'クラス', '場所', '距離', '回り']
 
 class Race_rote_app():
     def __init__(self):
         self.tk = Tk()
         self.tk.title('ウマ娘 レース計画ソフト')
-        self.tk.geometry('500x400')
+        self.tk.geometry('600x500')
 
-        txt = Entry(width=20)
-        txt.place(x=90, y=200)
+        self.txt = Entry(width=20)
+        self.txt.place(x=10, y=10)
 
-        self.df = pd.read_csv(PATH, index=['a', 'a', 'a', 'a', 'a', 'a', 'a'])
-        print(self.df)
-            
+        btn = Button(self.tk, text='検索', command=self.search_race)
+        btn.place(x=10, y=50)
+
+        self.text = StringVar()
+        self.text.set('')
+        self.label = Label(textvariable=self.text)
+        self.label.place(x=10, y=80)
+
+        self.read_race_data()
+
+    def read_race_data(self):
+        self.df_race = pd.read_csv(PATH, names=RACE_COLUMNS)
+    
+    def search_race(self):
+        en = self.txt.get()
+        s = self.df_race[self.df_race['レース名'].str.contains(en)]
+        self.text.set(s)
 
     def run(self):
         self.tk.mainloop()
